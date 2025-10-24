8759bbe6f9a053b45575155346b0e59e2f2fb33e
AHZA-DEV <kentdiyo@gmail.com>
2025-10-24 14:35:50 +0700

    Tugas membuat api

---
+++ backend/app/Http/Controllers/MemberController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Member;
+use Illuminate\Http\Request;
+
+class MemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $members = Member::all();
+        return response()->json([
+            'Member' => $members,
+            'message' => 'Data Berhasil diambil',
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:members',
+            'phone' => 'required',
+        ]);
+        $member = Member::create($request->all());
+        return response()->json($member, 201);
+    }
+
+    public function show($id)
+    {
+        $member = Member::findOrFail($id);
+        return response()->json($member, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $member = Member::findOrFail($id);
+        $member->update($request->all());
+        return response()->json($member, 200);
+    }
+
+    public function destroy($id)
+    {
+        Member::destroy($id);
+        return response()->json(null, 204);
+    }
+}
